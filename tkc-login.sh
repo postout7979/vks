4cf4f55b9834de39ba4583e08b80c5a43d125a87
postout7979 <76907027+postout7979@users.noreply.github.com>
2024-10-17 14:12:05 +0900

    Update tkc-login.sh

--- tkc-login.sh
+++ tkc-login.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+# Supervisor control plane VIP from Load balancer
 VSPHERE_WITH_TANZU_CONTROL_PLANE_IP=10.10.152.1
+# vSphere account that is registered to vSphere Namespace
 VSPHERE_WITH_TANZU_USERNAME=ichoi@vsphere.local
 VSPHERE_WITH_TANZU_PASSWORD=VMware1!
+# vSphere Namespace name
 VSPHERE_WITH_TANZU_NAMESPACE=ns-ichoi
+# Tanzu Kubernetes Cluster name
 VSPHERE_WITH_TANZU_TKC_NAME=beta-tkg01
+# Kuberctl binary path
 KUBECTL_PATH=/usr/bin/kubectl
 
 KUBECTL_VSPHERE_LOGIN_COMMAND=$(expect -c "
